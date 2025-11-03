edf288fc88d6c5e96714705680f4b3610ccccbc8
Yfmos <liambabin@sfr.fr>
2025-11-03 16:05:09 +0100

    TP2 POO

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,8 +1,12 @@
 package personnages;
 
+import objets.Equipement;
+
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -37,8 +41,36 @@ public class Romain {
 		this.force = force - forceCoup;
 	}
 	
+	public void sEquiper(Equipement equipement) {
+		if (nbEquipement == 2) {
+			System.out.println("Le soldat " + getNom() + " est déjà bien protégé !");
+		}
+		else {
+			if (nbEquipement==0) {
+				System.out.println("Le soldat " + getNom() + " s'équipe avec un " + equipement.toString() + ".");
+				equipements[0] = equipement;
+				nbEquipement ++;
+			}
+			else {
+				if (equipement == equipements[0]) {
+					System.out.println("Le soldat " + getNom() + " possède déjà un " + equipement.toString() + ".");
+				}
+				else {
+					System.out.println("Le soldat " + getNom() + " s'équipe avec un " + equipement.toString() + ".");
+					equipements[1] = equipement;
+					nbEquipement ++;
+				}
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus",6);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.BOUCLIER);
+
 	}
 
 }
